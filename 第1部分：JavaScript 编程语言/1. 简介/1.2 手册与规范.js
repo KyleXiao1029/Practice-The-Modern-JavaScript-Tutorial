ec8923b23b55bf4d8ff740c1320fe8db7a175ec6
却也 <1873181535@qq.com>
2025-02-17 14:59:45 +0800

    首次提交

---
+++ 第1部分：JavaScript 编程语言/1. 简介/1.2 手册与规范.js
@@ -0,0 +1,5 @@
+// ECMA-262 规范
+// https://tc39.es/ecma262/
+
+// MDN 手册
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
---
+++ 第1部分：JavaScript 编程语言/1. 简介/1.3 代码编辑器.js
@@ -0,0 +1,21 @@
+// IDE
+
+// Visual Studio Code
+// https://code.visualstudio.com/
+
+// WebStorm
+// https://www.jetbrains.com/webstorm/
+
+// 轻量编辑器
+
+// Sublime Text
+// https://www.sublimetext.com/
+
+// NotePad++
+// https://notepad-plus-plus.org/
+
+// Vim
+// https://www.vim.org/
+
+// Emacs
+// https://www.gnu.org/software/emacs/
